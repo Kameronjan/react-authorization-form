28d64b50ec5baf42e07689a8976d609d6ba85d56
Kameron <122124504+Kameronjan@users.noreply.github.com>
2023-07-29 23:44:23 -0400

    refactored and cleaned up code, good to push

--- public/contact.js
+++ public/contact.js
@@ -1,36 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function Contact() {
-  const password = 'swordfish';
+  const password = "swordfish";
   const [authorized, setAuthorized] = useState(false);
 
   function handleSubmit(e) {
     e.preventDefault();
-    const enteredPassword = e.target.querySelector('input[type="password"]').value;
+    const enteredPassword = e.target.querySelector(
+      'input[type="password"]'
+    ).value;
     const auth = enteredPassword === password;
     setAuthorized(auth);
   }
 
   const login = (
     <form action="#" onSubmit={handleSubmit}>
-      <input
-        type="password"
-        placeholder="Password"
-      />
-      <input
-        type="submit"
-      />
+      <input type="password" placeholder="Password" />
+      <input type="submit" />
     </form>
   );
 
   const contactInfo = (
     <ul>
-      <li>
-        client@example.com
-      </li>
-      <li>
-        555.555.5555
-      </li>
+      <li>client@example.com</li>
+      <li>555.555.5555</li>
     </ul>
   );
 
