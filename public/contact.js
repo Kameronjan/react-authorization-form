cec5456a99f46b0e2c235ef0eef6e17491c39571
Kameron <122124504+Kameronjan@users.noreply.github.com>
2023-07-29 23:11:02 -0400

    added ternary operator and changed value of authorized to true for the moment so that I can access the page

--- public/contact.js
+++ public/contact.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 function Contact() {
   const password = 'swordfish';
-  const [authorized, setAuthorized] = useState(false);
+  const [authorized, setAuthorized] = useState(true);
 
   function handleSubmit(e) {
     const enteredPassword = e.target.querySelector(
@@ -13,7 +13,7 @@ function Contact() {
 
   return (
       <div id="authorization">
-        <h1>Contact</h1>
+        {authorized ? <h1>Contact</h1> : <h1>Enter the Password</h1>}
         <ul>
           <li>
             client@example.com
